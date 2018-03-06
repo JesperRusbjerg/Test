b56316bdace6b6c88a4ffbb903a56cec117a2f42
Neytrax <cph-rh161@cphbusiness.dk>
2018-03-06 10:45:18 +0100

    new commit

--- GitTest/src/gittest/GitTest.java
+++ GitTest/src/gittest/GitTest.java
@@ -16,7 +16,7 @@ public class GitTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // Mette/Pernille/Ralle/Nikolai er seje 
+        // Mette/Pernille/Ralle/Nikolai/Jepser er seje
     }
     
 }
